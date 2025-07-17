beac7a7640f8dde300b3dd7b34c2c83e8da6318b
VaishnaviBunga <vaishnavibunga09@gmail.com>
2025-07-17 19:29:32 +0530

    mm

--- models/userModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-const userSchema = mongoose.Schema({
-  name: { type: String },
-  email: { type: String },
-  pass: { type: String },
-});
-// const user = mongoose.model("User", userSchema);
-
-export default mongoose.model("User", userSchema);
