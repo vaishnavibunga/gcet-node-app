ce21fca1a45db6eee1453a47a996ddca5a9bb2c6
VaishnaviBunga <vaishnavibunga09@gmail.com>
2025-07-18 10:14:04 +0530

    gg

--- dist/models/orderModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-const orderSchema = mongoose.Schema({
-  email: { type: String },
-  orderValue: { type: Number },
-  orderDate: { type: Date, default: Date.now },
-});
-// const product = mongoose.model("Product", productSchema);
-
-export default mongoose.model("Order", orderSchema);
